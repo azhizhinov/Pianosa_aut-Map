84a07a57f9a295b6bf92b840a1e481cdfdee75d3
azhizhinov <azhizhinov@mail.ru>
2015-07-24 23:32:19 +0300

    Space missed.

--- a3_epoch_server_settings/configs/maps/pianosa_aut.h
+++ a3_epoch_server_settings/configs/maps/pianosa_aut.h
@@ -1,4 +1,4 @@
-classPianosa_aut:Default
+class Pianosa_aut:Default
 {
 	worldSize=8192;
 	traderBlds[]={"Land_Kiosk_blueking_F","Land_FuelStation_Build_F","Land_CarService_F","Land_i_Garage_V1_F","Land_i_Shed_Ind_F","Land_i_Shop_01_V1_F"};
